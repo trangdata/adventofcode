5cfab2ed8d4bc2301e1aa06ee2d8d7a10208ce39
Trang Le <grixor@gmail.com>
2021-09-29 17:08:57 -0600

    day 14

--- 2019/day14.R
+++ 2019/day14.R
@@ -1,52 +1,148 @@
-source('utils.R')
+source("utils.R")
 library(tidyverse)
 
-ex <- get_aoc_input(14, 2019, Sys.getenv('COOKIE_PATH'))
+ex <- get_aoc_input(14, 2019, Sys.getenv("COOKIE_PATH"))
+## ----------------------------------
+# ex = '171 ORE => 8 CNZTR
+# 7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+# 114 ORE => 4 BHXH
+# 14 VRPVC => 6 BMBT
+# 6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+# 6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+# 15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+# 13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+# 5 BMBT => 4 WPTQ
+# 189 ORE => 9 KTJDG
+# 1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+# 12 VRPVC, 27 CNZTR => 2 XDBXC
+# 15 KTJDG, 12 BHXH => 5 XCVML
+# 3 BHXH, 2 VRPVC => 7 MZWV
+# 121 ORE => 7 VRPVC
+# 7 XCVML => 6 RJRHP
+# 5 BHXH, 4 VRPVC => 5 LTCX'
+#
+# ex = "10 ORE => 5 A
+# 2 A => 3 B
+# 7 B => 1 C
+# 7 B, 1 C => 1 FUEL"
+#
+# ex = '157 ORE => 5 NZVS
+# 165 ORE => 6 DCFZ
+# 44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+# 12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+# 179 ORE => 7 PSHF
+# 177 ORE => 5 HKGWZ
+# 7 DCFZ, 7 PSHF => 2 XJWVT
+# 165 ORE => 2 GPVTF
+# 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT'
+# # #
+# ex = '2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+# 17 NVRVD, 3 JNWZP => 8 VPVL
+# 53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+# 22 VJHF, 37 MNCFX => 5 FWMGM
+# 139 ORE => 4 NVRVD
+# 144 ORE => 7 JNWZP
+# 5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+# 5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+# 145 ORE => 6 MNCFX
+# 1 NVRVD => 8 CXFTF
+# 1 VJHF, 6 MNCFX => 4 RFSQX
+# 176 ORE => 6 VJHF'
+# #
+# ex = '9 ORE => 2 A
+# 8 ORE => 3 B
+# 7 ORE => 5 C
+# 3 A, 4 B => 1 AB
+# 5 B, 7 C => 1 BC
+# 4 C, 1 A => 1 CA
+# 2 AB, 3 BC, 4 CA => 1 FUEL'
+#
+# ex = "10 ORE => 10 A
+# 1 ORE => 1 B
+# 7 A, 1 B => 1 C
+# 7 A, 1 C => 1 D
+# 7 A, 1 D => 1 E
+# 7 A, 1 E => 1 FUEL"
+## ----------------------------------
+# part 1
 
+ex_ore <- paste(ex, "1 ORE => 1 ORE", sep = "\n")
 
-ex = '10 ORE => 10 A
-1 ORE => 1 B
-7 A, 1 B => 1 C
-7 A, 1 C => 1 D
-7 A, 1 D => 1 E
-7 A, 1 E => 1 FUEL'
-
-input0 <- strsplit(ex, '\n')[[1]] %>%
-  strsplit(' => ') %>%
+input0 <- strsplit(ex_ore, "\n")[[1]] %>%
+  strsplit(" => ") %>%
   do.call(rbind, .) %>%
   as.data.frame() %>%
-  `colnames<-`(c('material', 'result')) %>%
-  separate_rows(material, sep = ', ') %>%
-  separate(material, into = c('num_mat', 'mat'), sep = ' ') %>%
-  separate(result, into = c('num_res', 'res'), sep = ' ') %>%
-  mutate(num_mat = as.integer(num_mat),
-         num_res = as.integer(num_res)) %>%
-  {.}
+  `colnames<-`(c("material", "result")) %>%
+  separate_rows(material, sep = ", ") %>%
+  separate(material, into = c("num_mat", "mat"), sep = " ") %>%
+  separate(result, into = c("num_res", "res"), sep = " ") %>%
+  mutate(
+    num_mat = as.integer(num_mat),
+    num_res = as.integer(num_res),
+    across(c(mat, res), ~ fct_recode(
+      .x,
+      a = "VNFJQ", b = "TFQH", c = "RBFX", d = "ZWQX",
+      e = "VBHVQ", f = "DRWB", g = "NJFXK"
+    ) %>%
+      as.character())
+  )
 
-# resi = 'FUEL'
-dat = input0 %>%
-  filter(res %in% 'FUEL') %>%
-  select(num_mat, mat)
-sum_ores = 0
-first_materials = input0 %>%
-  filter(mat == 'ORE') %>%
-  pull(res)
+outs <- c("FUEL")
+temp <- input0 %>%
+  filter(res %in% outs) %>%
+  select(-num_res)
 
-repeat{
-  dat = input0 %>%
-    # filter(res %in% dat$mat) %>%
-    right_join(dat, by = c('res' = 'mat'))
+while (length(unique(temp$mat)) > 1) {
+  dat <- temp %>%
+    mutate(alone = mat %in% {
+      input0 %>%
+        filter(!res %in% outs) %>%
+        pull(mat)
+    })
 
-  if (nrow(dat) <= 1 && is.na(dat$mat))
-    break
-
-  dat = dat %>%
-    mutate(num_mat = num_mat.x * ceiling(num_mat.y/num_res)) %>%
-    # select(num_mat, mat) %>%
+  temp <- filter(dat, !alone) %>%
+    select(num_mat, mat) %>%
     group_by(mat) %>%
-    summarise(num_mat = sum(num_mat), .groups = 'drop')
+    summarise(num_mat = sum(num_mat), .groups = "drop") %>%
+    left_join(input0, by = c("mat" = "res")) %>%
+    mutate(num_mat = num_mat.y * ceiling(num_mat.x / num_res)) %>%
+    select(num_mat, mat = mat.y, res = mat) %>%
+    bind_rows(filter(dat, alone) %>% select(- alone))
 
-  sum_ores = sum_ores +
-    dat %>% filter(mat == 'ORE') %>% pull(num_mat) %>% sum()
+  outs <- unique(temp$res)
 }
-sum_ores
+
+sum(temp$num_mat)
+
+#-------
+# part 2
+input_trillion <- input0 %>%
+  mutate(across(c(num_mat, num_res), ~ ifelse(res == "FUEL", .x * 2876992, .x)))
+
+outs <- c("FUEL")
+temp <- input_trillion %>%
+  filter(res %in% outs) %>%
+  select(-num_res)
+
+while (length(unique(temp$mat)) > 1) {
+  dat <- temp %>%
+    mutate(alone = mat %in% {
+      input0 %>%
+        filter(!res %in% outs) %>%
+        pull(mat)
+    })
+
+  temp <- filter(dat, !alone) %>%
+    select(num_mat, mat) %>%
+    group_by(mat) %>%
+    summarise(num_mat = sum(num_mat), .groups = "drop") %>%
+    left_join(input0, by = c("mat" = "res")) %>%
+    mutate(num_mat = num_mat.y * ceiling(num_mat.x / num_res)) %>%
+    select(num_mat, mat = mat.y, res = mat) %>%
+    bind_rows(filter(dat, alone) %>% select(- alone))
+
+  outs <- unique(temp$res)
+}
+
+print(sum(temp$num_mat), digits = 16)
+
