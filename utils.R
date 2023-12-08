deff2da19f185cbab57665b656baeb5bffcc2ad0
let20 <trang.le@bms.com>
2023-12-08 08:44:45 -0500

    refine

--- utils.R
+++ utils.R
@@ -1,13 +1,11 @@
-library(magrittr)
 library(httr)
-get_aoc_input <- function(day, year = 2020, cookie, encoding = "UTF-8") {
+get_aoc_input <- function(day, year = 2023, cookie = Sys.getenv("TRANG_COOKIE"), encoding = "UTF-8") {
   GET(
-    paste0("https://adventofcode.com/", year, "/day/", day,
-           "/input"),
+    paste0("https://adventofcode.com/", year, "/day/", day, "/input"),
     set_cookies(session = cookie)
-  ) %>% content("text",
-                encoding = encoding) %>% trimws()
-
+  ) |>
+    content("text", encoding = encoding) |>
+    trimws()
 }
 
 bitsToInt <- function(x) sum(2^(which(rev(x))-1))
